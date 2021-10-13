44e5db6a6feac549a25200f1964864dd41f273ba
Dan Marshall <daniel.o.marshall@gmail.com>
2021-10-13 16:55:09 +0100

    Refactored for rubocop

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,25 +1,23 @@
 require 'pg'
 
 class Bookmarks
-
-  # attr_reader :list
-  
+ 
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect :dbname => 'bookmark_manager_test'
+      con = PG.connect dbname: 'bookmark_manager_test'
     else
-      con = PG.connect :dbname => 'bookmark_manager'
+      con = PG.connect dbname: 'bookmark_manager'
     end
 
     list = con.exec "SELECT * FROM bookmarks"
-    list.map { |bookmark| {url: bookmark['url'], title: bookmark['title']} }
+    list.map { |bookmark| { url: bookmark['url'], title: bookmark['title'] } }
   end
 
   def self.create(url, title)
     if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect :dbname => 'bookmark_manager_test'
+      con = PG.connect dbname: 'bookmark_manager_test'
     else
-      con = PG.connect :dbname => 'bookmark_manager'
+      con = PG.connect dbname: 'bookmark_manager'
     end
 
     con.exec "INSERT INTO bookmarks (url, title) VALUES ('#{url}','#{title}')"
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -9,15 +9,15 @@ describe Bookmarks do
 
     bookmarks = Bookmarks.all
 
-    expect(bookmarks).to include({title: 'Makers Academy', url:'http://www.makersacademy.com/'})
-    expect(bookmarks).to include({title: 'Destroy All Software', url:'http://www.destroyallsoftware.com/'})
-    expect(bookmarks).to include({title: 'Google', url: 'http://www.google.com/'})
+    expect(bookmarks).to include({ title: 'Makers Academy', url: 'http://www.makersacademy.com/' })
+    expect(bookmarks).to include({ title: 'Destroy All Software', url: 'http://www.destroyallsoftware.com/' })
+    expect(bookmarks).to include({ title: 'Google', url: 'http://www.google.com/' })
   end
 
   describe '#create' do
     it 'creates a new bookmark' do
       described_class.create('http://www.facebook.com', 'Facebook')
-      expect(described_class.all).to include({title: 'Facebook', url:'http://www.facebook.com'})
+      expect(described_class.all).to include({ title: 'Facebook', url: 'http://www.facebook.com' })
     end
   end
 end
